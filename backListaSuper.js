da690e2a6f3bc8444513ab80781002dcc9ef2add
JuanEndl <juanendl-_@hotmail.com>
2026-08-03 01:06:23 -0300

    metodo get

--- backListaSuper.js
+++ backListaSuper.js
@@ -1,16 +1,18 @@
-require('dotenv').config(); // carga las variables del .env
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
 
 const app = express();
-const PORT = 5000;
 
-// Middleware
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Conexión a MySQL usando pool
+// Conexión a MySQL
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -18,45 +20,49 @@ const db = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
-// Función para esperar a que MySQL esté listo
+// Esperar a que MySQL esté listo
 const waitForDb = () => {
   return new Promise((resolve) => {
     const check = () => {
       db.query("SELECT 1", (err) => {
         if (err) {
           console.log("Esperando a que MySQL esté listo...");
-          setTimeout(check, 2000); // reintenta cada 2 segundos
+          setTimeout(check, 2000);
         } else {
           resolve();
         }
       });
     };
+
     check();
   });
 };
 
-
-
-
-// Ruta para traer todos los productos
+// Obtener todos los productos
 app.get("/productos", (req, res) => {
-  db.query("select * from productos;", (err, results) => {
+  db.query("SELECT * FROM productos", (err, results) => {
     if (err) {
-      console.error("Error en la consulta:", err);
-      res.status(500).json({ error: "Error en la consulta a la base de datos" });
-    } else {
-      res.json(results);
+      console.error(err);
+      return res.status(500).json({
+        error: "Error al consultar la base de datos",
+      });
     }
+
+    res.json(results);
   });
 });
 
-// Esperar a que MySQL esté listo y luego arrancar servidor
+//TODO HACER EL POST 
+
+
+// Iniciar servidor
 waitForDb().then(() => {
-  console.log("MySQL está listo, arrancando servidor...");
+  console.log("MySQL está listo.");
+
   app.listen(PORT, () => {
-    console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
   });
 });
